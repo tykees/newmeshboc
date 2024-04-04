9c21abbd73fba07904e8c3e3479e4b76e75405d3
tykees <balogun.olu.sam@gmail.com>
2024-04-04 19:22:45 +0100

    typos

--- src/app/blog/steps-to-master-your-skill/page.jsx
+++ src/app/blog/steps-to-master-your-skill/page.jsx
@@ -9,7 +9,7 @@ const page = () => {
           <p className='text-3xl font-extrabold pb-10'>Steps to Master Your Skill: A Simple Guide to Getting Better</p>
         <Image className='w-auto' src={Learn} alt='post img'></Image>
         <p className='text-2xl mt-5 font-bold'>Introduction</p>
-          <p className='mt-5 text-justify'>Becoming really good at something takes time and effort. Whether you want to be great at your job, a hobby, or something new, there are steps you can take to get there. In this article, we'll talk about simple ways you can improve your skills and reach your goals.</p>
+          <p className='mt-5 text-justify'>Becoming really good at something takes time and effort. Whether you want to be great at your job, a hobby, or something new, there are steps you can take to get there. In this article, we&apos;ll talk about simple ways you can improve your skills and reach your goals.</p>
 
           <p className='text-2xl mt-5 font-bold'>Know What You Want:</p>
           <p className='mt-5 text-justify'>First, figure out what you want to achieve. What are your goals? Where do you see yourself in the future? Having clear goals helps you stay focused and gives you something to aim for.</p>
@@ -24,9 +24,9 @@ const page = () => {
           <p className='mt-5 text-justify'>Feedback from others can help you improve faster. Ask for advice from people who know about your skill, and listen to what they have to say. Use their feedback to make changes and get better.</p>
 
           <p className='text-2xl mt-5 font-bold'>Keep Learning:</p>
-          <p className='mt-5 text-justify'>Learning never stops. Keep exploring new ideas and techniques, and stay curious about your skill. Take classes, read books, or watch videos to learn more. The more you know, the better you'll become.</p>
+          <p className='mt-5 text-justify'>Learning never stops. Keep exploring new ideas and techniques, and stay curious about your skill. Take classes, read books, or watch videos to learn more. The more you know, the better you&apos;ll become.</p>
 
-          <p className='text-2xl mt-5 font-bold'>Don't Give Up:</p>
+          <p className='text-2xl mt-5 font-bold'>Don&apos;t Give Up:</p>
           <p className='mt-5 text-justify'>Getting really good at something takes time. Stay patient and keep going, even when things get tough. Celebrate your progress along the way, and remember that every step forward is a step closer to mastery.</p>
 
           <p className='text-2xl mt-5 font-bold'>Be Consistent:</p>
